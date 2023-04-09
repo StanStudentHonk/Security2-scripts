7ea0201b26bcf9389df94f1ffd0a176802d5b006
StanStudentHonk <74409816+StanStudentHonk@users.noreply.github.com>
2023-04-09 10:58:00 +0200

    add comments

--- paswordcracker.py
+++ paswordcracker.py
@@ -1,31 +1,20 @@
 import itertools
 import requests
 
-# Define your endpoint URL
+# Login endpoint
 url = "http://localhost:3333/api/auth/login"
 
-# Define your authorization token
-
-# Define your headers with the authorization token
 headers = {
     "Content-Type": "application/json"
 }
 
-# Define your request parameters (if any)
-
-
-# Make the API call with requests library
-# response = requests.get(url, headers=headers, params=params)
-# print(response.text)
-# password = 'a91Pso' # The password to crack
+## specificeer lengte van mogelijke wachtwoord
 password_length = 4
 
-
+## specificeer mogelijke caracters
 combinationslower = itertools.product("abcdefghijklmnopqrstuvwxyz", repeat=password_length)
-# combinationsDigit = itertools.product("0123456789", repeat=password_length)
 
-
-# Iterate through all combinations and check if they match the password
+# Probeert alle combinaties voor een email
 count = 0
 for guess in combinationslower:
     guess = ''.join(guess)
@@ -34,9 +23,6 @@ for guess in combinationslower:
      "password": guess
     }
     response = requests.post(url, headers=headers, json=params)
-    count = count + 1
-    print(count)
-    print(response.text)
     if response.status_code != 401:
         print(response.text)
         break
